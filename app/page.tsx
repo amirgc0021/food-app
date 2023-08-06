3ef8fda812cda45c35fecb5b6d028c099b134066
amirgc0021 <amirgc0021@gmail.com>
2023-08-07 00:26:26 +0300

    installing mongodb package and create a service to connect to local instance

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './page.module.css';
+import db from "@/services/db";
 
-export default function Home() {
+export default async function Home() {
+	const amir= await getData()
   return (
     <main className={styles.main}>
 			<h1>amir</h1>
     </main>
   )
+}
+
+const getData = async () => {
+	db("categories")
+	.then(async data => {
+		const list = await data.find({}).limit(1).toArray()
+		console.log(list)
+	})
+	.catch(console.error)
 }
---
+++ services/db/index.ts
@@ -0,0 +1,16 @@
+import { MongoClient } from 'mongodb';
+
+// Connection URL
+const url = 'mongodb://127.0.0.1:27017';
+const client = new MongoClient(url);
+let connection: MongoClient | null = null;
+
+export default async function main(collection: string) {
+	if (!connection) {
+		console.log('Connected successfully to server');
+		connection = await client.connect();
+	}
+	// Use connect method to connect to the server
+	const db = connection.db('food-app');
+	return db.collection(collection);
+};
